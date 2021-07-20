e069c3a49ef18de00e236a7df76b5986f020d8c9
MariaAga <mariaagaphonchev@gmail.com>
2021-07-20 11:19:42 +0200

    Fixes #32045 - Add start-end date pickers to job wizard

--- webpack/JobWizard/JobWizard.js
+++ webpack/JobWizard/JobWizard.js
@@ -7,7 +7,7 @@ import { translate as __ } from 'foremanReact/common/I18n';
 import history from 'foremanReact/history';
 import CategoryAndTemplate from './steps/CategoryAndTemplate/';
 import { AdvancedFields } from './steps/AdvancedFields/AdvancedFields';
-import { JOB_TEMPLATE } from './JobWizardConstants';
+import { JOB_TEMPLATE, repeatTypes } from './JobWizardConstants';
 import { selectTemplateError } from './JobWizardSelectors';
 import Schedule from './steps/Schedule/';
 import './JobWizard.scss';
@@ -16,6 +16,14 @@ export const JobWizard = () => {
   const [jobTemplateID, setJobTemplateID] = useState(null);
   const [category, setCategory] = useState('');
   const [advancedValues, setAdvancedValues] = useState({});
+  const [scheduleValue, setScheduleValue] = useState({
+    repeatType: repeatTypes.noRepeat,
+    repeatAmount: '',
+    starts: '',
+    ends: '',
+    isFuture: false,
+    isNeverEnds: false,
+  });
   const dispatch = useDispatch();
 
   const setDefaults = useCallback(
@@ -92,7 +100,12 @@ export const JobWizard = () => {
     },
     {
       name: __('Schedule'),
-      component: <Schedule />,
+      component: (
+        <Schedule
+          scheduleValue={scheduleValue}
+          setScheduleValue={setScheduleValue}
+        />
+      ),
       canJumpTo: isTemplate,
     },
     {
